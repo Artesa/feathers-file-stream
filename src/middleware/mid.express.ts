b7090f6a955538f47591eb22ad6ed122fa344d9e
fratzinger <22286818+fratzinger@users.noreply.github.com>
2022-11-01 10:22:34 +0100

    chore: remove code snippet

--- src/middleware/mid.express.ts
+++ src/middleware/mid.express.ts
@@ -33,50 +33,6 @@ export const expressHandleStreams = (options: ExpressHandleStreamsOptions) => {
   };
 };
 
-const handleFiles = async (req, res, next) => {
-  const { method } = req;
-  if (method === "POST" || method === "PATCH") {
-    if (!req.files || !Array.isArray(req.files) || req.files.length === 0) {
-      next();
-    }
-
-    const { associatedServicePath } = req.body;
-
-    let { categoryId, associatedId } = req.body;
-
-    categoryId = tryParseInt(categoryId, null);
-    associatedId = tryParseInt(associatedId, null);
-    // TODO: Check if associatedServicePath exists
-
-    // eslint-disable-next-line prefer-destructuring, no-undef
-    const files: Express.Multer.File[] = req.files;
-
-    const filesObjects: FileObject[] = files.map((file) => {
-      return {
-        originalName: path.basename(file.originalname),
-        fileName: path.basename(file.originalname),
-        buffer: file.buffer,
-        fileSize: file.size,
-        mimeType: file.mimetype,
-        associatedServicePath,
-        associatedId,
-        categoryId
-      };
-    });
-
-    if (!req.feathers.files) req.feathers.files = filesObjects;
-    req.body = filesObjects;
-
-    next();
-  } else if (method === "GET") {
-    next();
-  } else if (method === "DELETE") {
-    next();
-  } else {
-    next();
-  }
-};
-
 export const expressMiddlewareStream =
   () => (req: Request, res: Response, next: NextFunction) => {
     if (req.method !== "GET" || !("data" in res)) {
